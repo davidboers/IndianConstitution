2fa2feeba55e7cba9fb24c85116c475614530bef
davidboers <kwmmineccraft@gmail.com>
2025-04-20 02:09:41 -0400

    List renumbering

--- static/src/auto-show-changes.js
+++ static/src/auto-show-changes.js
@@ -1,3 +1,4 @@
 const text = Array.from(document.querySelectorAll('span.ins'))
-     .concat(Array.from(document.querySelectorAll('span.del')));
+     .concat(Array.from(document.querySelectorAll('span.del')))
+     .concat(Array.from(document.querySelectorAll('li.renum')));
 text.forEach(span => span.classList.add('changes'));

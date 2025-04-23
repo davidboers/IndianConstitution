d9bae3afce7060196256953ad282cda7ceadcba2
davidboers <kwmmineccraft@gmail.com>
2025-04-23 15:08:02 -0400

    Added `getIndexedLinks`

--- static/src/utils.js
+++ static/src/utils.js
@@ -3,3 +3,11 @@ export function parseHTMLDoc(html) {
     const doc = parser.parseFromString(html, 'text/html');
     return doc;
 }
+
+export function getIndexedLinks(html) {
+    const doc = parseHTMLDoc(html);
+    const links = Array.from(doc.querySelectorAll('a'))
+        .filter(link => link.innerText != '../' && !link.innerText.includes('http-server'))
+        .map(link => link.innerText);
+    return links;
+}

e4cef307c55ced77efc63070028d31b3d145c037
davidboers <kwmmineccraft@gmail.com>
2026-04-15 22:22:05 -0400

    References to Schedules

--- static/data/reference_finder.py
+++ static/data/reference_finder.py
@@ -2,21 +2,62 @@ import os
 import json
 import re
 
-def find_references(html):
-    refs = re.findall(r'article [0-9A-Z]+', html, flags = re.IGNORECASE)
+FULL_SCHEDULES = r'(?:Second|Third|Fourth|Fifth|Sixth|Seventh|Eighth|Ninth|Tenth|Eleventh|Twelfth)+ Schedule'
 
-    group_refs = re.findall(r'articles [0-9A-Z]+ and [0-9A-Z]+', html, re.IGNORECASE) + \
-                 re.findall(r'articles [0-9A-Z]+, [0-9A-Z]+ and [0-9A-Z]+', html, re.IGNORECASE) + \
-                 re.findall(r'articles [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+ and [0-9A-Z]+', html, re.IGNORECASE) + \
-                 re.findall(r'articles [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+ and [0-9A-Z]+', html, re.IGNORECASE) + \
-                 re.findall(r'articles [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+, [0-9A-Z]+ and [0-9A-Z]+', html, re.IGNORECASE)
+def find_references(html):
+    refs = re.findall(r'article [0-9]+[A-Z]*', html, flags = re.IGNORECASE)
+
+    # Group article references
+
+    group_refs = re.findall(r'articles [0-9]+[A-Z]*(?:, [0-9]+[A-Z]*)* and [0-9]+[A-Z]*', html, re.IGNORECASE) + \
+                 re.findall(r'articles [0-9]+[A-Z]*, [0-9]+[A-Z]*, [0-9]+[A-Z]*, [0-9]+[A-Z]* and (?:article )[0-9]+[A-Z]*', html, re.IGNORECASE) # Article 366(26B) seems to be unique
 
     for ref in group_refs:
-        numbers = re.findall(r'[0-9A-Z]+', ref, re.IGNORECASE)
+        numbers = re.findall(r'[0-9]+[A-Z]*', ref, re.IGNORECASE)
         for number in numbers:
             refs.append('article ' + number)
 
+    # Schedule references other than the First Schedule
+
+    sch_refs = re.findall(FULL_SCHEDULES, html, re.IGNORECASE)
+    refs.extend(sch_refs)
+
+    # First Schedule (pre-1956 references)
+
+    fst_sch_refs = re.findall(r'Part(?:s)? [A-D](?:, [A-D])*(?: and [A-D])? of the First Schedule', html, re.IGNORECASE)
+
+    for ref in fst_sch_refs:
+        headings = re.findall(r'Part(?:s)? ([A-D])(?:, ([A-D]))*(?: and ([A-D]))?', ref, re.IGNORECASE)
+        headings = [heading for tp in headings for heading in tp if heading] # Flattening trick
+        for heading in headings:
+            refs.append(f'part {heading} of the first schedule')
+
     return refs
+
+def extract_title(html, fullpath):
+    title = re.search(r'<title>([a-z0-9\s]+) — Constitution of India</title>', html, re.IGNORECASE)
+
+    if title is None:
+        print(f'Warning: {fullpath} has no title.')
+        return 'Untitled'
+    
+    title = title.group(1).lower()
+
+    # Generalize titles of Schedules other than the pre-1956 First Schedule
+
+    if 'schedule' in title:
+        schedule_ref = re.search(FULL_SCHEDULES, title, re.IGNORECASE)
+
+        if schedule_ref is not None:
+            title = schedule_ref.group(0)
+
+        else:
+            schedule_ref = re.search(r'Part [A-D] of the First Schedule', title, re.IGNORECASE)
+
+            if schedule_ref is not None:
+                title = schedule_ref.group(0)
+
+    return title
 
 def reference_finder(path):
     pairs = []
@@ -30,10 +71,13 @@ def reference_finder(path):
                 with open(fullpath, 'r', encoding = 'utf-8') as versionfile:
                     html = versionfile.read()
 
-                    title = html.split('<title>')[1].split(' — Constitution of India</title>')[0].lower()
-                    print(title)
+                    title = extract_title(html, fullpath)
+                    
                     name = html.split('<p class="art">')[1].split('</p>')[0].replace('—', '') # Strip node marking and hyphen
                     name = re.sub(r'\s+', ' ', name)
+                    if re.search(r'First Schedule', title, re.IGNORECASE):
+                        name = re.sub(r'(?<=Part [A-D])\.', r' of the First Schedule.', name)
+
                     reference_links[title] = {
                         "name": name,
                         "path": artpath
@@ -55,6 +99,10 @@ def reference_finder(path):
         for (a, b) in pairs
         if a in reference_links and b in reference_links
     ]
+
+    for (a, b) in pairs:
+        if b not in reference_links:
+            print(f'No link for: {b}/ In {a}')
 
     extern = json.load(open('static/data/external-references.json', 'r'))
 

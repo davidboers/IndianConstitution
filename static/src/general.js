dc5835e9e487b25797b43fdf64b2b7f78b4278d3
davidboers <kwmmineccraft@gmail.com>
2026-04-20 15:08:18 -0400

    Clause name italicization

--- static/src/general.js
+++ static/src/general.js
@@ -1,23 +1,26 @@
-import { lang } from './utils.js';
+import { lang } from '/static/src/utils.js';
 
 $('header').load(`/${lang}/header.html`);
 $('#main-nav').load(`/${lang}/main-nav.html`);
 $('footer').load(`/${lang}/footer.html`);
 
+const pattern = /\(([0-9a-zA-Z]+)\)/g;
+
 if (document.querySelector('.art-holder')) {
-    $('.art-holder').each(function() {
-        $(this).load($(this).attr('name'));
+    $('.art-holder').each(function () {
+        $(this).load($(this).attr('name'),
+
+            // Clause references
+            () => {
+                $('.art-holder p').each(function () {
+                    let html = $(this).html();
+
+                    html = html.replace(pattern, function (match, inner) {
+                        return `(<i>${inner}</i>)`;
+                    });
+
+                    $(this).html(html);
+                });
+            })
     });
 }
-
-// Clause references
-
-let pattern = /\(([a-z]|ii)\)/;
-Array.from(document.querySelectorAll('p')).map(p => {
-    while (p.innerHTML.search(pattern) != -1) {
-        let html = p.innerHTML;
-        let index = html.search(pattern);
-        let newhtml = `(<i>${html.substr(index + 1, 1)}</i>)`;
-        p.innerHTML = html.replace(pattern, newhtml);
-    }
-});

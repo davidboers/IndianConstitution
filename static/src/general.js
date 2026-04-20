c2feaaae415e3c17a0d747e0395efa01d75783d5
davidboers <kwmmineccraft@gmail.com>
2026-04-19 20:20:15 -0400

    Header, footer, and navbar

--- static/src/general.js
+++ static/src/general.js
@@ -1,3 +1,17 @@
+import { lang } from './utils.js';
+
+$('header').load(`/${lang}/header.html`);
+$('#main-nav').load(`/${lang}/main-nav.html`);
+$('footer').load(`/${lang}/footer.html`);
+
+if (document.querySelector('.art-holder')) {
+    $('.art-holder').each(function() {
+        $(this).load($(this).attr('name'));
+    });
+}
+
+// Clause references
+
 let pattern = /\(([a-z]|ii)\)/;
 Array.from(document.querySelectorAll('p')).map(p => {
     while (p.innerHTML.search(pattern) != -1) {

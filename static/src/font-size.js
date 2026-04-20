fbc61d625a59d2cb53641b84abc90730bcb50731
davidboers <kwmmineccraft@gmail.com>
2026-04-19 20:25:50 -0400

    jquery references

--- static/src/font-size.js
+++ static/src/font-size.js
@@ -4,10 +4,10 @@ const root = document.documentElement;
 const $toggle_small = $('#font-size-small');
 const $toggle_large = $('#font-size-large');
 
-toggle_small.addEventListener('click', () => {
-    root.style.setProperty('--base-font-size', toggle_small.css('fontSize'));
+$toggle_small.on('click', () => {
+    root.style.setProperty('--base-font-size', $toggle_small.css('fontSize'));
 });
 
-toggle_large.addEventListener('click', () => {
-    root.style.setProperty('--base-font-size', toggle_large.css('fontSize'));
+$toggle_large.on('click', () => {
+    root.style.setProperty('--base-font-size', $toggle_large.css('fontSize'));
 });

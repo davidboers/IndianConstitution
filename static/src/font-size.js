ce91b9b0d18e0d6eb3059caee1892a6be0c9704f
davidboers <kwmmineccraft@gmail.com>
2026-04-20 13:13:46 -0400

    Saves configed font size as a cookie

--- static/src/font-size.js
+++ static/src/font-size.js
@@ -4,10 +4,21 @@ const root = document.documentElement;
 const $toggle_small = $('#font-size-small');
 const $toggle_large = $('#font-size-large');
 
+function setSize(option) {
+    root.style.setProperty('--base-font-size', option.css('fontSize'));
+}
+
+let cookie = decodeURIComponent(document.cookie).split(';').find((cookie) => cookie.startsWith('font-size='));
+if (cookie && cookie.startsWith('font-size=large')) {
+    setSize($toggle_large);
+}
+
 $toggle_small.on('click', () => {
-    root.style.setProperty('--base-font-size', $toggle_small.css('fontSize'));
+    setSize($toggle_small);
+    document.cookie = 'font-size=small; path=/';
 });
 
 $toggle_large.on('click', () => {
-    root.style.setProperty('--base-font-size', $toggle_large.css('fontSize'));
+    setSize($toggle_large);
+    document.cookie = 'font-size=large; path=/';
 });

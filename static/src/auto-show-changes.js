e38918b3bd55bb6891e2d518cde6bd515cba8722
davidboers <kwmmineccraft@gmail.com>
2026-04-22 22:03:12 -0400

    Removed `auto-show-changes.js`; already deprecated

--- static/src/auto-show-changes.js
+++
@@ -1,6 +0,0 @@
-/**
- * @fileoverview This file is redundant as its previous features have been moved to './toggle-changes.js'.
- * @deprecated Remove from .html files.
- */
-
-console.warn('[Deprecation Warning]: auto-show-changes.js is redundant.');

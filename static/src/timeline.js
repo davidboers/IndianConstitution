f0ac2a08ee3c538239e01449be6b40e5ac15e4af
davidboers <kwmmineccraft@gmail.com>
2026-05-01 16:57:38 -0400

    Timeline switcher using site index

--- static/src/timeline.js
+++ static/src/timeline.js
@@ -1,4 +1,5 @@
 import { formatArticleText } from "./general.js";
+import { getLangIndex, parseHTMLDoc } from "./utils.js";
 
 const begin = new Date(1949, 10, 26); // 10 = November (0 indexed)
 const end = new Date();
@@ -7,92 +8,70 @@ function getFrac(date) {
     return (date.getTime() - begin.getTime()) / (end.getTime() - begin.getTime());
 }
 
-function changeVersion($label, link) {
-    $('.art-holder').attr('name', link);
-    $('.art-holder').each(function () {
-        $(this).load(link, () => formatArticleText($(this)));
-    });
+function loadVersion(path, $holder) {
+    // Do not import using article.content, as this is the raw template, not the complete version.
+    $holder.load(path, () => formatArticleText($holder));
+}
+
+function changeVersion($label, slug, url) {
+    const $holder = $('.art-holder');
+    $holder.attr('data-version', slug);
+    loadVersion(url, $holder);
     $('.timeline-label-bold').removeClass('timeline-label-bold');
     $label.addClass('timeline-label-bold');
 }
 
-function amendmentNumber(link) {
-    if (link.includes('/')) {
-        link = link.substr(link.lastIndexOf('/') + 1);
-    }
-    if (link === 'original.html') {
-        return 'Orig.';
-    }
-    if (link.endsWith('.html')) {
-        link = link.replace('.html', '');
-        if (/a[0-9]+/.test(link)) {
-            return link.replace('a', '');
-        } else {
-            return link.toUpperCase();
-        }
-    }
+function amendmentNumber(slug) {
+    if (slug === 'original') return 'Orig.';
+    return (/a[0-9]+/.test(slug)) ? slug.replace('a', '') : slug.toUpperCase();
+
 }
 
 void function () {
+    const $holder = $('.art-holder');
+    loadVersion(`version/${$holder.attr('data-version')}.html`, $holder);
+
     const $timeline = $('#timeline');
     const coefficient = $timeline.width() / $('body').width() * 100;
 
-    const $top_labels = $('<div></div>');
-    const $bottom_labels = $('<div></div>');
-    $top_labels.addClass('timeline-label-container');
-    $bottom_labels.addClass('timeline-label-container');
+    const $top_labels = $('<div class="timeline-label-container"></div>');
+    const $bottom_labels = $('<div class="timeline-label-container"></div>');
 
-    async function addAmendment(link) {
-        try {
-            const response = await fetch(link);
-            const html = await response.text();
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
-            const $effectiveDate = $(doc).find('#effective-date');
-            const day = parseInt($effectiveDate.find('#eff-day').html());
-            const month = parseInt($effectiveDate.find('#eff-month').html()) - 1; // 0 indexed
-            const year = parseInt($effectiveDate.find('#eff-year').html());
+    function addAmendment(entry) {
+        const $effectiveDate = $(parseHTMLDoc(entry.content)).find('#effective-date');
+        const day = parseInt($effectiveDate.find('#eff-day').html());
+        const month = parseInt($effectiveDate.find('#eff-month').html()) - 1; // 0 indexed
+        const year = parseInt($effectiveDate.find('#eff-year').html());
 
-            const date = new Date(year, month, day);
-            const frac = getFrac(date) * coefficient;
+        const date = new Date(year, month, day);
+        const frac = getFrac(date) * coefficient;
 
-            const selected = $('.art-holder').attr('name') === link;
-            const $label = $(`<span class="timeline-label"><span>${year.toString()}<br>${amendmentNumber(link)}</span></span>`);
-            $label.css('left', `${frac}%`);
-            if (selected) $label.addClass('timeline-label-bold');
-            $label.on('click', () => changeVersion($label, link));
-            $label.attr('name', link);
+        const selected = $('.art-holder').attr('data-version') === entry.slug;
+        const $label = $(`<span class="timeline-label"><span>${year.toString()}<br>${amendmentNumber(entry.slug)}</span></span>`);
+        $label.css('left', `${frac}%`);
+        if (selected) $label.addClass('timeline-label-bold');
+        $label.on('click', () => changeVersion($label, entry.slug, entry.url));
+        $label.attr('data-version', entry.slug);
 
-            return { date, $label };
-        } catch (error) {
-            console.error('Error fetching version html:', error);
-        }
+        return { date, $label };
     }
 
-    fetch('./version')
-        .then(response => response.text())
-        .then(html => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
-            const $links = Array.from($(doc).find('a')
-                .map((_, link) => 'version/' + link.innerHTML)
-                .filter((_, href) => href.endsWith('.html')));
+    getLangIndex()
+        .then(index => {
+            index.filter(entry => entry.article == window.location.pathname)
+                .map(addAmendment)
+                .sort((a1, a2) => a1.date - a2.date)
+                .forEach(amendment => {
+                    if ($bottom_labels.children().length > $top_labels.children().length) {
+                        $top_labels.append(amendment.$label);
 
-            Promise.all($links.map(addAmendment)).then(amendments =>
-                amendments
-                    .filter(Boolean)
-                    .sort((a1, a2) => a1.date - a2.date)
-                    .forEach(amendment => {
+                    } else {
+                        $bottom_labels.append(amendment.$label);
 
-                        if ($bottom_labels.children().length > $top_labels.children().length) {
-                            $top_labels.append(amendment.$label);
-                        } else {
-                            $bottom_labels.append(amendment.$label);
-                        }
+                    }
 
-                    }))
-        })
-        .catch(error => console.error('Error fetching directory listing:', error));
+                });
+        });
 
     const $axis = $('<div class="timeline-axis"></div>');
     const $line = $('<div class="timeline-line"></div>');
